e9bc3f3023e37b60784c54ab7eaefc3c1ed8d641
RipStepz <ripstepz@gmail.com>
2023-11-27 17:42:15 -0300

    avance2

--- trivia_app/urls.py
+++ trivia_app/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 
 # trivia_app/urls.py
 from django.urls import path
-from .views import inicio, pregunta, finalizar_juego
+from .views import inicio, preguntas, finalizar_juego
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path('preguntas/<int:numero_pregunta>/', pregunta, name='preguntas'),
+    path('preguntas/<int:numero_pregunta>/', preguntas, name='preguntas'),
     path('finalizar-juego/', finalizar_juego, name='finalizar_juego'),
 ]
